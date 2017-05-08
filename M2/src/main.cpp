23050be4c4f3615dd75ed23e7e85cf3889a0fcf6
Joan Moreno Fuentes <joanmf.011@gmail.com>
2017-05-08 16:31:31 +0200

    Depth Shader Implemented

--- M2/src/main.cpp
+++ M2/src/main.cpp
@@ -14,6 +14,7 @@
 #include "cameras/perspective.h"
 
 #include "shaders/intersectionshader.h"
+#include "shaders/depthshader.h"
 
 void buildSceneSphere(Camera* &cam, Film* &film,
                       std::vector<Shape*>* &objectsList,
@@ -126,7 +127,8 @@ int main()
     // Declare the shader
     Vector3D bgColor(0.0, 0.0, 0.0); // Background color (for rays which do not intersect anything)
     Vector3D intersectionColor(1,0,0);
-    Shader *shader = new IntersectionShader (intersectionColor, bgColor);
+    //Shader *shader = new IntersectionShader (intersectionColor, bgColor);
+	Shader *shader = new DepthShader(Vector3D(0.4, 1, 0.4), 8, bgColor);
 
     // Declare pointers to all the variables which describe the scene
     Camera *cam;
