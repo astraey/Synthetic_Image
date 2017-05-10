4099b90c4690979aa2e7cd30aee7f2f70e6f59a6
Joan Moreno Fuentes <joanmf.011@gmail.com>
2017-05-10 16:36:49 +0200

    Developing DirectShader

--- M2/src/shaders/directshader.cpp
+++ M2/src/shaders/directshader.cpp
@@ -15,13 +15,30 @@ DirectShader::DirectShader(Vector3D color_, double maxDist_, Vector3D bgColor_) 
 Vector3D DirectShader::computeColor(const Ray &r, const std::vector<Shape *> &objList, const std::vector<PointLightSource> &lsList) const
 {
 	Intersection its;
+	Vector3D I;
 	Vector3D Distance;
+
+	int nL = lsList.size();
+
+	if (Utils::getClosestIntersection(r, objList, its))
+	{
+		for (int i = 0; i < nL; i++)
+		{
+
+			I = I.operator+(lsList[i].getIntensity(its.itsPoint));
+
+		}
+	}
+	else
+	{
+		return bgColor;
+	}
 
 	if (Utils::getClosestIntersection(r, objList, its))
 	{
 		Distance = its.itsPoint.operator-(r.o);
 		double dist = Distance.length();
-		
+
 		if (dist >= maxDist)
 		{
 
@@ -30,14 +47,23 @@ Vector3D DirectShader::computeColor(const Ray &r, const std::vector<Shape *> &ob
 		}
 		else
 		{
-		
-			return color*(1.0 - dist/maxDist);
+
+			for (int i = 0; i < nL; i++)
+			{
+				Vector3D intensity = lsList[i].getIntensity(its.itsPoint);
+				Vector3D reflectance = its.shape->getMaterial().getReflectance(); // FALTA PASAR N, WI, WO DE PHONG::GETREFLECTANCE
+				I = I.operator+(Utils::multiplyPerCanal(intensity, reflectance));
+
+			}
 
 		}
+
+		return I;
 
 	}
 	else
 	{
 		return bgColor;
 	}
+	
 }
